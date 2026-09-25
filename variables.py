d1d5d9eeee0e1b876d579481656242c585e83d6a
Leo-git-12351 <l.herreraparedes@colegiovirgendeeuropa.edu.es>
2026-09-25 09:56:36 +0200

    variable operations

---
+++ variables.py
@@ -0,0 +1,17 @@
+#declaration of a string variable
+name="Victor"
+#declaration of a integer variable
+age=23
+#
+print(name)
+print(age)
+
+age=45
+print(age)
+
+print(type(name))
+
+print(type(0.25))
+print(type(0.025))
+print(type(10))
+print(type(age))
